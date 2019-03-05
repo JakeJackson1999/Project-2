677515567d88f1b255df61c74e6707309349e793
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-03-05 10:49:59 -0600

    initialized variables

--- MesoInherit.java
+++ MesoInherit.java
@@ -1,5 +1,9 @@
 public abstract class MesoInherit {
 	String city = MesoStation.getStID();
-	
+	 double firstAscii = 0;
+	 double secondAscii = 0;
+	 double thirdAscii = 0;
+	 double fourthAscii = 0;
+	 
 	//TODO
 }
