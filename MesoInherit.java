fe24749cf12ecd9596da93556daed7e25f792add
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-03-05 10:53:11 -0600

    set characters to ASCII

--- MesoInherit.java
+++ MesoInherit.java
@@ -5,5 +5,10 @@ public abstract class MesoInherit {
 	 double thirdAscii = 0;
 	 double fourthAscii = 0;
 	 
+	 firstAscii = (int)city.subString(0, 1);
+	 secondAscii = (int)city.subString(1, 2);
+	 thirdAscii = (int)city.subString(2, 3);
+	 fourthAscii = (int)city.subString(3, 4);
+	 
 	//TODO
 }
