f725075e64ab93022e350a31ccab2bfa2d1f0e79
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-03-05 11:18:20 -0600

    moved code from inherit to abstract

--- MesoAbstract.java
+++ MesoAbstract.java
@@ -1,4 +1,20 @@
 public abstract class MesoAbstract 
 {
-   //TODO	
+	private MesoStation StID;
+	
+	public MesoAbstract(MesoStation StID) {
+		this.StID = StID;
+	}
+	
+	double firstAscii = 0;
+	double secondAscii = 0;
+	double thirdAscii = 0;
+	double fourthAscii = 0;
+	 
+	firstAscii = (int)StID.subString(0, 1);
+	secondAscii = (int)StID.subString(1, 2);
+	thirdAscii = (int)StID.subString(2, 3);
+	fourthAscii = (int)StID.subString(3, 4);
+	
+	//TODO	
 }
