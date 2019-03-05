8d302b4dc3246a70dc1f722bb3014ff99617da21
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-03-05 11:13:20 -0600

    fixed MesoInherit

--- MesoInherit.java
+++ MesoInherit.java
@@ -1,14 +1,20 @@
-public abstract class MesoInherit {
-	String city = MesoStation.getStID();
-	 double firstAscii = 0;
-	 double secondAscii = 0;
-	 double thirdAscii = 0;
-	 double fourthAscii = 0;
+public class MesoInherit extends MesoAbstract {
+	
+	private MesoStation StID;
+	
+	public MesoInherit(MesoStation StID) {
+		this.StID = StID;
+	}
+	
+	double firstAscii = 0;
+	double secondAscii = 0;
+	double thirdAscii = 0;
+	double fourthAscii = 0;
 	 
-	 firstAscii = (int)city.subString(0, 1);
-	 secondAscii = (int)city.subString(1, 2);
-	 thirdAscii = (int)city.subString(2, 3);
-	 fourthAscii = (int)city.subString(3, 4);
-	 
+	firstAscii = (int)StID.subString(0, 1);
+	secondAscii = (int)StID.subString(1, 2);
+	thirdAscii = (int)StID.subString(2, 3);
+	fourthAscii = (int)StID.subString(3, 4);
+	
 	//TODO
 }
