3385dd3b33c891196be889c89b8f45b14512b4c4
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-03-05 11:17:44 -0600

    removed code from class, moved to MesoAbstract

--- MesoInherit.java
+++ MesoInherit.java
@@ -1,20 +1,6 @@
 public class MesoInherit extends MesoAbstract {
 	
-	private MesoStation StID;
 	
-	public MesoInherit(MesoStation StID) {
-		this.StID = StID;
-	}
-	
-	double firstAscii = 0;
-	double secondAscii = 0;
-	double thirdAscii = 0;
-	double fourthAscii = 0;
-	 
-	firstAscii = (int)StID.subString(0, 1);
-	secondAscii = (int)StID.subString(1, 2);
-	thirdAscii = (int)StID.subString(2, 3);
-	fourthAscii = (int)StID.subString(3, 4);
 	
 	//TODO
 }
