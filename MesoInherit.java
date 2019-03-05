60a3c95dcda31fa3355d88240cef22d8caad90dd
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-03-05 11:40:45 -0600

    started calAverage

--- MesoInherit.java
+++ MesoInherit.java
@@ -1,4 +1,11 @@
 public class MesoInherit extends MesoAbstract {
+
+	public static int[] calAverage() {
+		double average = (firstAscii + secondAscii + thirdAscii + fourthAscii) / 4;
+		int ceiling = (int)Math.ceil(average);
+		int floor = (int)Math.floor(average);
+		
+	}
 	
 	
 	
