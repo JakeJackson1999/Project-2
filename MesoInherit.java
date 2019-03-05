0cd1c62155ac9f4d6890dfc86157f6c31319f964
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-03-05 11:54:42 -0600

    fixed the calAverage array

--- MesoInherit.java
+++ MesoInherit.java
@@ -1,14 +1,18 @@
 public class MesoInherit extends MesoAbstract {
 
-	public static int[] calAverage() {
+	public int[] calAverage() {
 		double average = (firstAscii + secondAscii + thirdAscii + fourthAscii) / 4;
 		int ceiling = (int)Math.ceil(average);
 		int floor = (int)Math.floor(average);
 		
 		calAverage[0] = ceiling;
 		calAverage[1] = floor;
-		calAverage[2] = average;
+		calAverage[2] = (int)average;
+		
+		return calAverage;
 	}
+	
+	
 	
 	
 	
