9ec1add75caef19c8493d5dae5a871c1e04a35a7
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-03-07 12:29:45 -0600

    finished documentation

--- MesoAbstract.java
+++ MesoAbstract.java
@@ -4,10 +4,12 @@ public abstract class MesoAbstract {
 	//create protected string for later char use
 	protected String StID;
 	
+	//set StID to the given StID from Main.java
 	public MesoAbstract(MesoStation mesostation) {
 		StID = mesostation.getStID();
 	}
 	
+	//created array for use in MesoInherit
 	int[] calAverage = new int[3];//TODO	
 }
 
