dff3c600c9119d612aa220f8e191123dc1021dfa
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-03-07 12:36:25 -0600

    finished documentation

--- MesoInherit.java
+++ MesoInherit.java
@@ -14,26 +14,31 @@ public class MesoInherit extends MesoAbstract {
 		char c = StID.charAt(2);
 		char d = StID.charAt(3);
 		
+		//cast each char to the ascii value
 		double firstAscii = (int)a;
 		double secondAscii = (int)b;
 		double thirdAscii = (int)c;
 		double fourthAscii = (int)d;
 		
+		//find average and set floor and ceiling
 		double average = (firstAscii + secondAscii + thirdAscii + fourthAscii) / 4;
 		int ceiling = (int)Math.ceil(average);
 		int floor = (int)Math.floor(average);
 		
+		//set each to correct array index
 		calAverage[0] = ceiling;
 		calAverage[1] = floor;
 		calAverage[2] = (int)Math.ceil(average);
 		
+		//return array
 		return calAverage;
 	}
 	
 	public char letterAverage() {
+		//get the average
 		double average = calAverage[0];
 		
-		
+		//return the letter
 		return (char)average;
 	}
 	
