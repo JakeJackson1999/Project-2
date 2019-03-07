4cf5596aeb3ec7fc4f1c196d3aae54d8b59ad1f8
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-03-07 12:28:18 -0600

    added some documentation

--- MesoAbstract.java
+++ MesoAbstract.java
@@ -1,6 +1,7 @@
 import java.util.Arrays;
 
 public abstract class MesoAbstract {
+	//create protected string for later char use
 	protected String StID;
 	
 	public MesoAbstract(MesoStation mesostation) {
