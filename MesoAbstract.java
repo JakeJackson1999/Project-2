028802e73fb630187c6a617aa4f738b3b397a065
Jake Jackson <jake.r.jackson-1@ou.edu>
2019-03-07 12:26:41 -0600

    Cleaned up code

--- MesoAbstract.java
+++ MesoAbstract.java
@@ -7,17 +7,6 @@ public abstract class MesoAbstract {
 		StID = mesostation.getStID();
 	}
 	
-	/*char[] chars = StID.toCharArray();
-	char a = StID.charAt(0);
-	char b = StID.charAt(1);
-	char c = StID.charAt(2);
-	char d = StID.charAt(3);
-	
-	double firstAscii = (int)a;
-	double secondAscii = (int)b;
-	double thirdAscii = (int)c;
-	double fourthAscii = (int)d;*/
-	
 	int[] calAverage = new int[3];//TODO	
 }
 
